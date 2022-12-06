5899d5be8b27a5e10937dfa6a349638941cbdf8b
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-12-06 12:30:21 -0500

    Update images

--- P5_heart_sketches/Heart_curve_5/sketch.js
+++ P5_heart_sketches/Heart_curve_5/sketch.js
@@ -3,7 +3,7 @@
 // https://youtu.be/oUBAi9xQ2X4
 
 // Heart curve formula adapted from https://mathworld.wolfram.com/HeartCurve.html
-//  r = 2 - 2 * sin(a) + sin(a)*(pow(abs(cos(a)), 0.5)/(sin(a) + 1.4))
+//  r =  sin(a)*pow(abs(cos(a)), 0.5) / (sin(a)+7/5) ) + 2 * sin(a) + 2; (not sure if * or +)
 
 const heart = [];
 let a = 0;
@@ -19,7 +19,6 @@ function draw() {
   let c0 = color(0);
   let c1 = color(146,83,161);
   let c2 = color('#F063A4');
-  
 
   let  col2 = setGradientL(0, 0, 400, 450, c1, c2, X_AXIS);
   let  col3 = setGradientR(400, 0, 750, 450, c2, c1, X_AXIS);
@@ -35,10 +34,9 @@ function draw() {
   }
   endShape();
 
-  const sc = 60;
-  const r = 2 - 2 * sin(a) + sin(a)*(pow(abs(cos(a)), 0.5)/(sin(a) + 1.4));
-  const x = sc * r * cos(a);
-  const y = sc * r * sin(a);
+  // const r = 450 * (sin(a)*pow(abs(cos(a)), 0.05) / (sin(a)+7/5) );
+  // const x =  r * cos(a);
+  // const y = 1.25 * r * abs(pow(sin(a), 0.25));
 
   heart.push(createVector(x, y));
   
